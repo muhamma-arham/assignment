069e9dabb0d5b48103d89c8277e9b625f74cef44
muhamma-arham <161346796+muhamma-arham@users.noreply.github.com>
2024-05-28 14:49:33 +0300

    assignment

---
+++ project21/main.js
@@ -0,0 +1,11 @@
+/// create two object
+var book = {
+    name: 'ESSENTIAL TYPESCRIPT',
+    PRICE: 450
+};
+var apple = {
+    name: 'apple',
+    price: 200
+};
+console.log('book name: ${book.name}, price: $${book.name}');
+console.log('book name: ${apple.name}, price: $${apple.name}');
---
+++ project21/main.ts
@@ -0,0 +1,15 @@
+// interface item {
+//     name:string
+//     price:number
+// }
+// /// create two object
+// const book: item ={
+//     name: 'ESSENTIAL TYPESCRIPT'
+//     PRICE:450
+// }
+// const apple: item ={
+//     name:'apple'
+//     price:200
+// }
+// console.log('book name: ${book.name}, price: $${book.name}')
+// console.log('book name: ${apple.name}, price: $${apple.name}')
