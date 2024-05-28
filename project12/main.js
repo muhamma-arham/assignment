64cdd5f1c10675dc79a0f27824dfb29dfff06c4f
muhamma-arham <161346796+muhamma-arham@users.noreply.github.com>
2024-05-28 14:41:24 +0300

    assignment

---
+++ project12/main.js
@@ -0,0 +1,4 @@
+var members = ['azam', 'maaz', 'saim', 'ibrahim', 'umar'];
+for (var i = 0; i < members.length; i++) {
+    console.log(members[i]);
+}
---
+++ project12/main.ts
@@ -0,0 +1,4 @@
+let members: string[] = ['azam','maaz','saim','ibrahim','umar'];
+for(let i=0; i<members.length; i++){
+    console.log(members[i]);
+}
