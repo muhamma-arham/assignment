e4ba588ce5d8ba7851d5c60ae4fb473e0ca71f6e
muhamma-arham <161346796+muhamma-arham@users.noreply.github.com>
2024-05-28 14:38:31 +0300

    Add files via upload

---
+++ project09,10/main.ts
@@ -0,0 +1,12 @@
+//exercise09
+
+//var favoriteNumber: number = 4 ;
+//console.log(`my favorite numer is ${favoriteNumber}`);
+
+//exercise10
+
+//my name is arham
+//dated:17/4/24
+// this program will run simple code just like hello world
+console.log('hello world simple program');
+
---
+++ project09,10/program2.ts
@@ -0,0 +1,6 @@
+//exercise10
+
+//my name is arham
+//dated:17/4/24
+//this program will multiplication
+console.log(5 * 2);
