c127ce5bfb827ae70d062f735b1d7e2829caabfc
muhamma-arham <161346796+muhamma-arham@users.noreply.github.com>
2024-05-28 14:48:24 +0300

    assignment

---
+++ project20/main.js
@@ -0,0 +1,6 @@
+var language = ['English', 'urdu', 'turkish', 'arabic', 'french'];
+console.log('list of languages:');
+for (var _i = 0, language_1 = language; _i < language_1.length; _i++) {
+    var top_1 = language_1[_i];
+    console.log(top_1);
+}
---
+++ project20/main.ts
@@ -0,0 +1,5 @@
+let languages : string [] = ['English' , 'urdu' , 'turkish' , 'arabic' , 'french'];
+console.log('list of languages:')
+for(let top of language){
+    console.log(top);
+}
