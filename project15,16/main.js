31af8ffed5787d865b5165b638fe8276c59c80fd
muhamma-arham <161346796+muhamma-arham@users.noreply.github.com>
2024-05-28 14:44:17 +0300

    assignment

---
+++ project15,16/main.js
@@ -0,0 +1,21 @@
+//Exercise:14
+//let guest_list = ['orhan', 'osman', 'noyan', 'rayyan'];
+//for (var i = 0; i < guest_list.length; i++) {
+//    console.log('Respected Sir/Madam ' + guest_list[i]);
+//}
+//Exercise:15
+var guest_list = ["ali", "farhan", "ibrahim", "raza"];
+for (var i = -0; i < guest_list.length; i++) {
+    console.log("Respected Sir/Madam " + guest_list[i] + ",\nWe invited you on dinner tomorrow.\nThank You\n");
+}
+var not_present = "farhan";
+var neww_guest = "arham";
+for (var i = 0; i < guest_list.length; i++) {
+    console.log("Respected Sir/Madam " + guest_list[i] + '\nWe invited you on dinner tomorrow.\nThank You\n');
+}
+//Exercise:16
+guest_list.unshift('shaheen', 'sarfaraz', 'rizwan');
+for (var i = 0; i < guest_list.length; i++) {
+    console.log('Respected Sir/Madam ' + guest_list[i] + ',\nWe invited you on diner tomorrow. \nThank you\n');
+}
+//Exercise:17
---
+++ project15,16/main.ts
@@ -0,0 +1,34 @@
+//Exercise:14
+
+//let guest_list = ['orhan', 'osman', 'noyan', 'rayyan'];
+//for (var i = 0; i < guest_list.length; i++) {
+//    console.log('Respected Sir/Madam ' + guest_list[i]);
+//}
+
+//Exercise:15
+
+let guest_list : string [] = ["ali" , "farhan" , "ibrahim" , "raza"];
+//for (let i=-0; i<guest_list.length; i++){
+//    console.log("Respected Sir/Madam " + guest_list[i] + ",\nWe invited you on dinner tomorrow.\nThank You\n")
+//}
+let not_present : string = "farhan";
+let neww_guest : string = "arham";
+guest_list[1]
+//for(let i=0; i<guest_list.length; i++){
+//    console.log("Respected Sir/Madam " + guest_list[i] + '\nWe invited you on dinner tomorrow.\nThank You\n')
+//}
+
+//Exercise:16
+
+guest_list.unshift('shaheen','sarfaraz','rizwan');
+//for(let i=0; i<guest_list.length;  i++){
+ //   console.log('Respected Sir/Madam ' +guest_list[i] + ',\nWe invited you on diner tomorrow. \nThank you\n')
+//}
+
+//Exercise:17
+
+console.log('\n Unfortunetely we can not arrange big table , Only two people allow. ')
+while(guest_list){
+    let remove_guest = guest_list.pop();
+    console.log
+}    
