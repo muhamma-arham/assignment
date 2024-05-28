c300808d89d73e766d9c287e235087ecaadddc82
muhamma-arham <161346796+muhamma-arham@users.noreply.github.com>
2024-05-28 14:43:14 +0300

    assignment

---
+++ project14/main.ts
@@ -0,0 +1,5 @@
+let guest_list : string [] = ['ali' , 'farhan' , 'raza' , 'ishal'];                             //Revision
+for(let i=0; i<guest_list.length; i++){
+    console.log('Respected Sir/Madam ' + guest_list[i] + ' ,\nWe inivited you on dinner\n');
+}
+export{guest_list}
