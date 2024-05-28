b9a044e7f78f9f05f8588e05e9761f3a83056993
muhamma-arham <161346796+muhamma-arham@users.noreply.github.com>
2024-05-28 14:36:35 +0300

    Add files via upload

---
+++ project03,4,5,6,7,8/main.ts
@@ -0,0 +1,45 @@
+
+//console.log(17 + 4);//Addition
+//console.log(16 - 9);//Subtraction
+//console.log(14 * 7);//Multipilication
+//console.log(40 / 3);//Divition
+
+//let layer = 5
+//let apples = 5
+//let power = layer ** 2;
+//console.log(power);
+
+
+
+//let apple = 5
+//let bag = 2
+//let reminder = apple % 2;
+//console.log(reminder);
+
+// program1
+//let index = 10;
+//++index; //10+1
+//++index; //11+1
+//++index; //12+1
+//++index; //13+1
+//++index; //14+1
+//console.log(index);
+// ++ ka matlab jo bhi current value hai usme 1 ka increment kardo
+// unary matlab 1,1 kar k implement karna
+
+// program2
+
+//let index = 15 ;
+// //                  11     +       12    + 6 +       11  -2  =   53 answer
+//let result = ++index + ++index + 6 + --index -2;
+//console.log(result);
+
+
+// new topic assignment operator
+// agar humme program mein koi cheez assign
+// karni hai to use kehte hai hum assign operator
+
+//let num1 : number = 20 ;
+// // 20 + 2 = 22;
+//num1 += 22;
+//console.log(num1);
