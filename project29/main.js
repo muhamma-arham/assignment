9546368b2c9981e37c2f9e7a9e2ce18d1d9396f8
muhamma-arham <161346796+muhamma-arham@users.noreply.github.com>
2024-05-28 14:58:30 +0300

    assignment

---
+++ project29/main.js
@@ -0,0 +1,7 @@
+var favorite_fruit = ['apple', 'orange', 'grapes'];
+if (favorite_fruit.includes('apple')) {
+    console.log('I really like apple!');
+}
+if (favorite_fruit.includes('orange')) {
+    console.log('I really like orange!');
+}
---
+++ project29/main.ts
@@ -0,0 +1,9 @@
+let favorite_fruit : string [] = ['apple','orange','grapes' ]
+
+if(favorite_fruit.includes('apple')){
+    console.log('I really like apple!')
+}
+
+if(favorite_fruit.includes('orange')){
+    console.log('I really like orange!')
+}
