233c0994aa46f0aa2f19c516eba8d95195552106
muhamma-arham <161346796+muhamma-arham@users.noreply.github.com>
2024-05-28 14:56:50 +0300

    assignment

---
+++ project28/main.js
@@ -0,0 +1,13 @@
+var age = 13;
+if (age < 2) {
+    console.log("this person is a baby");
+}
+else if (age >= 2 && 4) {
+    console.log("person is a toddler");
+}
+else if (age = 13 && 13) {
+    console.log("person is a kid.");
+}
+else {
+    console.log("person is a elder.");
+}
---
+++ project28/main.ts
@@ -0,0 +1,20 @@
+// let ages : number = 13; //revision
+// if(age<2){
+//     console.log("this person is a baby")
+// }
+
+
+
+// else if(age>=2 && <4){
+//     console.log("person is a toddler")
+// }
+
+
+
+// else if(age=13 && 13){
+//     console.log("person is a kid.")
+// }
+
+// else{
+//     console.log("person is a elder.")
+// }
