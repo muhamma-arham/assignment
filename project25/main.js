cea43a88783c23c149dfa755ac4b36e018843a54
muhamma-arham <161346796+muhamma-arham@users.noreply.github.com>
2024-05-28 14:53:58 +0300

    assignment

---
+++ project25/main.js
@@ -0,0 +1,13 @@
+//Create a variable called alien_color     revision
+var alien_color = "green";
+// Write an if statement to test whether teh alien's colr is green . if its
+//print a message that the player just earned 5 points.
+if (alien_color == "green") {
+    console.log("the player just earned 5 points. ");
+}
+// Write one version of this program that passes the if test and another that
+// (The version that fails will have no output. )
+alien_color = "red";
+if (alien_color == "green") {
+    console.log("the playeer just earned 5 points");
+}
